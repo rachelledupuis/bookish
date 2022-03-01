636994a6242125b7c1b16bdd5f78acc4bbcd7b15
Sam James <samjojames@gmail.com>
2022-03-01 16:03:40 +0000

    Create model view controller for booklist

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,34 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult BookList()
+    {
+        var books = new List<Book>{
+            new Book{
+                Title = "Harry Potter And The Prisoner Of Azkaban",
+                YearPublished = 1999,
+                Author = "JK Rowling",
+                ImageUrl = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1630547330l/5._SY475_.jpg",
+                Blurb = "Sirius Black turns out to be a good guy despite his name and house"
+            },
+            new Book{
+                Title = "Harry Potter And The Chamber of Secrets",
+                YearPublished = 1997,
+                Author = "JK Rowling",
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51mFoFmu0EL._SX335_BO1,204,203,200_.jpg",
+                Blurb = "Harry talks to some snakes"
+            },
+            new Book{
+                Title = "Harry Potter And The Deathly Hallows",
+                YearPublished = 2006,
+                Author = "JK Rowling",
+                ImageUrl = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1474171184l/136251._SY475_.jpg",
+                Blurb = "Harry finds some hawcruxes, whatever they are"
+            },
+        };
+        return View(books);
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
