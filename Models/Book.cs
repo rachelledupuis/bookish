e594e5b90411f137627b893a1bba1be6c2fee61d
Sam James <samjojames@gmail.com>
2022-03-01 15:46:47 +0000

    Create Book Model

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+namespace bookish.Models
+{
+    public class Book
+    {
+        public string? Title { get; set; }
+
+        public string? Author { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? Blurb { get; set; }
+    }
+}
