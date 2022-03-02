f25207fd26f163417b10696c78f341b918310777
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-03-02 09:56:32 +0000

    Add Member class

---
+++ Models/Member.cs
@@ -0,0 +1,12 @@
+namespace bookish.Models
+{
+    public class Member
+    {
+        public string? Name { get; set; }
+        
+        public DateTime? BirthDate { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
+
