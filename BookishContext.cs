d2830f7018b5f79ca14d25d31caa7b8b2c71139e
Sam James <samjojames@gmail.com>
2022-03-03 14:26:45 +0000

    create Member model

--- BookishContext.cs
+++ BookishContext.cs
@@ -7,6 +7,7 @@ namespace bookish
     {
         public DbSet<BookDbModel> Books { get; set; }
         public DbSet<AuthorDbModel> Authors { get; set; }
+        public DbSet<MemberDbModel> Members { get; set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
             optionsBuilder.UseSqlServer(@"Server=localhost;Database=BookishDB;Trusted_Connection=True;TrustServerCertificate=True;");
--- Repositories/MemberRepo.cs
+++ Repositories/MemberRepo.cs
@@ -1,28 +1,32 @@
 using bookish.Models.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace bookish.Repositories
 {
     public class MemberRepo
     {
+
+        private BookishContext _context = new BookishContext();
         public List<MemberDbModel> GetAllMembers()
         {
-            return new List<MemberDbModel>
-            {
-                new MemberDbModel
-                {
-                    Id = 1,
-                    Name = "Rachelle Dupuis",
-                    Email = "rachelle.dupois@softwire.com",
-                    BirthDate = new DateTime(1993,1,1)
-                },
-                new MemberDbModel
-                {
-                    Id = 2,
-                    Name = "Sam James",
-                    Email = "sam.james@softwire.com",
-                    BirthDate = new DateTime(1991,1,1)
-                }
-            };
+            return _context.Members.ToList();
+            // return new List<MemberDbModel>
+            // {
+                // new MemberDbModel
+                // {
+                //     Id = 1,
+                //     Name = "Rachelle Dupuis",
+                //     Email = "rachelle.dupois@softwire.com",
+                //     BirthDate = new DateTime(1993,1,1)
+                // },
+                // new MemberDbModel
+                // {
+                //     Id = 2,
+                //     Name = "Sam James",
+                //     Email = "sam.james@softwire.com",
+                //     BirthDate = new DateTime(1991,1,1)
+                // }
+            // };
         }
     }
 }
